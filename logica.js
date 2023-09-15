901124f36f4ebed371f33af58c515cd7cc210b60
ELECTRODUNO <74607247+ELECTRODUNO@users.noreply.github.com>
2023-09-15 15:08:03 +0200

    Update logica.js

--- logica.js
+++ logica.js
@@ -1,7 +1,1 @@
-const menuHaburguer = document.querySelector('.menu-hamburger');
-const iconBars = menuHaburguer.querySelector("i");
-const menu = document.querySelector('.manu');
 
-menuHaburguer.addEventListener("click", () => {
-    menu.classList.toggle('menu-responsive');
-})
